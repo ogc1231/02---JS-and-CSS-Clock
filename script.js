fb1fffe65d59f46ae48ac96de538c466b3c753d9
Oliver Craigie <ogc@live.ie>
2024-02-12 12:46:36 +0000

    Get seconds, minutes and hours

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const minHand = document.getElementsByClassName("min-hand");
 const hourHand = document.getElementsByClassName("hour-hand");
 
 const currentDate = new Date();
+const seconds = currentDate.getSeconds();
+const minutes = currentDate.getMinutes();
+const hours = currentDate.getHours();
