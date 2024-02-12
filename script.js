b71ac80700d2bf9e74c893cb6dfc5dfaac6dfa58
Oliver Craigie <ogc@live.ie>
2024-02-12 13:16:22 +0000

    Change colour of hands

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ const secHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
-// const currentDate = new Date();
-// const seconds = currentDate.getSeconds();
-// const minutes = currentDate.getMinutes();
-// const hours = currentDate.getHours();
-
 function setTime() {
   const currentDate = new Date();
-  console.log(currentDate);
+
   const seconds = currentDate.getSeconds();
   const minutes = currentDate.getMinutes();
   const hours = currentDate.getHours();
+
   const secDegrees = (seconds / 60) * 360 + 90;
   const minDegrees = (minutes / 60) * 360 + 90;
   const hourDegrees = (hours / 12) * 360 + 90;
