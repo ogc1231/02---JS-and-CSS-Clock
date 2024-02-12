c13aa57cc7454f25c14bd50a77a645aa9f7c7ee7
Oliver Craigie <ogc@live.ie>
2024-02-12 12:54:23 +0000

    create setTime function and get seconds in degrees

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const secHand = document.getElementsByClassName("second-hand");
 const minHand = document.getElementsByClassName("min-hand");
 const hourHand = document.getElementsByClassName("hour-hand");
 
-const currentDate = new Date();
-const seconds = currentDate.getSeconds();
-const minutes = currentDate.getMinutes();
-const hours = currentDate.getHours();
+// const currentDate = new Date();
+// const seconds = currentDate.getSeconds();
+// const minutes = currentDate.getMinutes();
+// const hours = currentDate.getHours();
+
+function setTime() {
+  const currentDate = new Date();
+  const seconds = currentDate.getSeconds();
+  const secDegrees = (seconds / 60) * 360;
+  console.log(seconds);
+}
+
+setInterval(setTime, 1000);
