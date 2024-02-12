4579988d6947e97e2f31b1a79a1423f93aebb16a
Oliver Craigie <ogc@live.ie>
2024-02-12 13:08:46 +0000

    Add hours and connect to hours hand

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ const hourHand = document.querySelector(".hour-hand");
 
 function setTime() {
   const currentDate = new Date();
+  console.log(currentDate);
   const seconds = currentDate.getSeconds();
   const minutes = currentDate.getMinutes();
+  const hours = currentDate.getHours();
   const secDegrees = (seconds / 60) * 360 + 90;
   const minDegrees = (minutes / 60) * 360 + 90;
+  const hourDegrees = (hours / 12) * 360 + 90;
 
   secHand.style.transform = `rotate(${secDegrees}deg)`;
   minHand.style.transform = `rotate(${minDegrees}deg)`;
-  console.log(seconds);
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setTime, 1000);
