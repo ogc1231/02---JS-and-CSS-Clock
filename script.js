1f5fb3f062d3e2576d179f6fa0ee225ce10681e2
Oliver Craigie <ogc@live.ie>
2024-02-12 12:43:02 +0000

    Add currentDate and select clock hand elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const secHand = document.getElementsByClassName("second-hand");
+const minHand = document.getElementsByClassName("min-hand");
+const hourHand = document.getElementsByClassName("hour-hand");
+
+const currentDate = new Date();
